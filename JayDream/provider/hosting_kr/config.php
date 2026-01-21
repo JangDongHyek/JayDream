6eb63dd61518bfca76512b3e041a29c3c70ffc5c
jangdonghyek <jangdonghyek@gmail.com>
2026-01-21 10:26:40 +0900

    260121

--- JayDream/provider/hosting_kr/config.php
+++ JayDream/provider/hosting_kr/config.php
@@ -2,5 +2,5 @@
 return [
     "success" => true,
 
-    "api_key" => "Urnp1t3pXlateBaVqd3UF48Ym81cf9CQ6dI0rbnD",
+    "api_key" => "",
 ];
--- JayDream/provider/portone/config.php
+++ JayDream/provider/portone/config.php
@@ -7,18 +7,18 @@ return [
 
     //공통
     "mid" => "IP06G",
-    "channel_key" => "channel-key-2026500a-0361-4953-963e-5c37ea8b17cd",
-    "test_channel_key" => "channel-key-a203c438-a175-47c6-9e18-2410d7f8093f",
-    "test_mid" => "T0000",
+    "channel_key" => "",
+    "test_channel_key" => "",
+    "test_mid" => "",
 
     //v1
-    "v1_code" => "imp34338245", // 고객사 식별코드
-    "v1_api_key" => "0648357731618364",
-    "v1_api_secret" => "6GHLH2iLGLzYhkGFrkPiIsH1W3kNiLlps3b95tULfuKwPqcgL1zuugwqkQdDCz24CddIH8uWDettFFKr",
+    "v1_code" => "", // 고객사 식별코드
+    "v1_api_key" => "",
+    "v1_api_secret" => "",
 
     //v2
-    "store_id" => "store-3029863c-088e-4a28-a0ad-eccbb8b64c10",
-    "secret_key" => "CuAk4uCHbDLY2Up9TBufmwQjpO5pw3ybkbVruKnGjfzfTqyDNjQb77Xd8Q3Jre2Q7Qtvk8zTen5WZB30",
+    "store_id" => "",
+    "secret_key" => "",
 
 
 ];
