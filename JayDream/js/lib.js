1ade9e438b5ee3d674ec7b6c25093dc5458e8920
jangdonghyek <jangdonghyek@gmail.com>
2026-03-20 18:41:25 +0900

    260320

--- JayDream/js/lib.js
+++ JayDream/js/lib.js
@@ -429,4 +429,10 @@ class JayDreamLib {
         }
     }
 
+    isEmpty(val) {
+        if (Array.isArray(val)) return val.length === 0;
+        if (val && typeof val === 'object') return Object.keys(val).length === 0;
+        return true;
+    }
+
 }
